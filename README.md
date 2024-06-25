3de935a5c4aabb99306ee918ea45217df0025274
B.NAVEEN BHARATHI <144258519+bnaveenbharathi@users.noreply.github.com>
2024-06-25 11:53:57 +0530

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# miniblog
+# NextGen Mini Blog
+
+NextGen is a mini blog application built with Flask. It allows users to create, update, delete, and view blog posts. The application includes an admin dashboard for managing posts and user authentication for login and logout functionality.
+
+## Features
+
+- **Create Post**: Admin can create new blog posts.
+- **Update Post**: Admin can update existing blog posts.
+- **Delete Post**: Admin can delete blog posts.
+- **View Post**: Users can view blog posts.
+- **Admin Dashboard**: Controls for managing posts.
+- **User Authentication**: Login and logout functionality for admin.
+
+
+## Installation
+
+1. **Clone the repository**:
+
+    ```sh
+    git clone https://github.com/yourusername/NextGen-MiniBlog.git
+    cd NextGen-MiniBlog
+    ```
+
+2. **Create a virtual environment**:
+
+    ```sh
+    python -m venv venv
+    ```
+
+3. **Activate the virtual environment**:
+
+    - On Windows:
+      ```sh
+      venv\Scripts\activate
+      ```
+    
+
+4. **Install dependencies**:
+
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+5. **Set up the database**:
+   - Make sure you have MySQL installed and running.
+   - Create a database named `blog`.
+   - Update the database connection settings in `app.py` if necessary.
+
+6. **Run the application**:
+
+    ```sh
+    python app.py
+    ```
+
+    The application will be accessible at `http://127.0.0.1:5000`.
+
+## Usage
+
+1. **Home Page**: View the latest blog posts.
+2. **Admin Dashboard**: Access the admin dashboard by logging in.
+3. **Login**: Use the login page to authenticate as an admin.
+4. **Signup**: Register a new admin user.
+5. **Create Post**: Use the admin dashboard to create new blog posts.
+6. **Update Post**: Use the admin dashboard to update existing blog posts.
+7. **Delete Post**: Use the admin dashboard to delete blog posts.
+8. **View Full Article**: Click on a blog post to view the full article.
+9. **Logout**: Use the logout functionality to end the admin session.
+
+## Configuration
+
+- **Database Settings**: Update the MySQL connection settings in `app.py` to match your database configuration.
+
+```python
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "blog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+```
+
+##Demo Images
+![image](https://github.com/bnaveenbharathi/miniblog/assets/144258519/034d2117-d6d5-403f-8bc2-6d8aea23e124)
+
+![image](https://github.com/bnaveenbharathi/miniblog/assets/144258519/749dcb1f-0b72-43c5-80b4-5f3ccc8a21d3)
+
+
+
--- db/blog.sql
+++ db/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2024 at 07:34 AM
+-- Generation Time: Jun 25, 2024 at 08:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 7.2.17
 
@@ -29,13 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `title` varchar(300) NOT NULL,
-  `content` text NOT NULL,
-  `author_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
   `CREATE_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `img_path` varchar(500) DEFAULT NULL
+  `img_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `CREATE_at`, `update_at`, `img_path`) VALUES
+(1, 'Breakthrough in Quantum Computing: Scientists Achieve Room-Temperature Quantum Entanglement', 'In an unprecedented leap forward, researchers at the Quantum Computing Institute have successfully demonstrated room-temperature quantum entanglement. This groundbreaking achievement could revolutionize the field of quantum computing, potentially making current technologies obsolete.\r\n\r\nDr. Emily Carter, lead scientist on the project, stated, \"This discovery opens up new possibilities for practical quantum computers that can operate in everyday environments. Our findings will pave the way for more accessible and efficient quantum technologies.\"\r\n\r\nThe team\'s work focuses on using a novel type of qubit that remains stable at higher temperatures, unlike traditional qubits that require extremely cold environments. This advancement could significantly reduce the costs and complexity associated with quantum computing, bringing us closer to a future where quantum computers are a part of our daily lives.\r\n\r\nIn an unprecedented leap forward, researchers at the Quantum Computing Institute have successfully demonstrated room-temperature quantum entanglement. This groundbreaking achievement could revolutionize the field of quantum computing, potentially making current technologies obsolete.\r\n\r\nDr. Emily Carter, lead scientist on the project, stated, \"This discovery opens up new possibilities for practical quantum computers that can operate in everyday environments. Our findings will pave the way for more accessible and efficient quantum technologies.\"\r\n\r\nThe team\'s work focuses on using a novel type of qubit that remains stable at higher temperatures, unlike traditional qubits that require extremely cold environments. This advancement could significantly reduce the costs and complexity associated with quantum computing, bringing us closer to a future where quantum computers are a part of our daily lives.', '2024-06-25 06:16:49', '2024-06-25 06:16:49', '/static/uploads/new.png');
 
 -- --------------------------------------------------------
 
@@ -51,6 +57,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `create_at`) VALUES
+(25, 'mark', '$2b$12$/QVF5zvFAXsonjaQUrc6k.5hrLX4BUHLXMqVxRJDoGC2SRXn18BzW', '2024-06-25 06:01:00');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -58,8 +71,7 @@ CREATE TABLE `users` (
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `author_id` (`author_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -76,23 +88,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
